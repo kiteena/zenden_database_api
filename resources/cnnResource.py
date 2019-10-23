cf142528d5cb37590d7cba093fa97d4059891983
kiteena <kristinamilkovich@gmail.com>
2019-10-22 18:55:28 -0700

    Updated CNN and Prediction endpoints. Lots of work left for improving models. Updated swagger docs for endpoint changes.

--- resources/cnnResource.py
+++ resources/cnnResource.py
@@ -2,17 +2,38 @@ from flask_restful import Resource
 from flask import request
 from deep_learning.cnn import CNN
 from urllib.parse import unquote
+from config import AccessKeyID, SecretAccessKey
+import boto3
+import os
+import base64
 
 cnn = CNN()
+folder_path = 'deep_learning/serialized_models/data/'
+
 class CNNResource(Resource): 
     def get(self):
         try: 
             args = request.args
-            folder_path = args['folder_path']
-            folder_path = unquote(folder_path)
-            folder_path = folder_path.lstrip('\"')
-            folder_path = folder_path.rstrip('\"')
+            filenames = unquote(args['filenames'])
+            filenames = [f.strip('\' ') for f in filenames.split(',')]
+            print(filenames)
+            # download files
+            for f in filenames: 
+                if not f: 
+                    continue
+                s3 = boto3.client ('s3', aws_access_key_id=AccessKeyID, aws_secret_access_key=SecretAccessKey)
+                try: 
+                    s3.download_file('zenden-cnn',f'{f}', folder_path + f'{f}')
+                except Exception as e:
+                    raise Exception('File not found in AWS bucket. Please verify file was uploaded.') 
+
+            # make predictions
             data = cnn.makePredictions(folder_path)
+            # remove downloaded files 
+            for f in filenames:
+                if not f: 
+                    continue
+                os.remove(folder_path + f'{f}')
             return {'status': 'success', "data": data}, 200 
         except Exception as e: 
             return {'status': str(e)}, 400
--- resources/predictionsResource.py
+++ resources/predictionsResource.py
@@ -3,41 +3,41 @@ from flask import request
 from models.userModel import User, UserSchema
 from models.matchModel import Match, MatchSchema
 from models.houseModel import House, HouseSchema
-from deep_learning.recommender import Predictions
 import json
 import cv2
+import datetime
 
 user_schema = UserSchema(only=['user_id'])
 houses_schema = HouseSchema(many=True)
 matches_schema = MatchSchema(many=True)
-preds = Predictions()
 
 class PredResource(Resource): 
     def get(self):
         try: 
             args = request.args
             user_id = args['user_id']
+            predicted_houses = []
 
-        # temp hack until nearest neighbor problem resolved
-            data = User.query.first()
-            sim_user = user_schema.dumps(data)
-            sim_user_id = json.loads(sim_user)['user_id']
-            
-            matches = Match.query.with_entities(Match.house_id).all()
-            matches1 = [m for m, in matches]
-            houses = House.query.with_entities(House.house_id).filter(House.house_id.in_(matches1)).limit(100).all()
-            houses1 =[h for h, in houses]
-            results = preds.makePredictions(houses1, sim_user_id)
-            results=[]
-            if results:
-                predicted_houses = House.query.filter(House.house_id.in_(results)).limit(25).all()
-            else:
-                predicted_houses = House.query.limit(25).all()          
+            find_user = User.query.filter_by(user_id = user_id).first()
 
-            # not sure if problem is on my side or Marshmallow, but json coming back double-encoded
+            # if the user has rated houses, find their predictions
+            if find_user: 
+                find_user_json = user_schema.dumps(find_user)
+                find_user_id = json.loads(find_user_json)['user_id']
+                matches = Match.query.filter_by(user_id=find_user_id, viewed=False).with_entities(Match.house_id).limit(20).all()
+                matches1 = [m for m, in matches]
+                predicted_houses = House.query.filter(House.house_id.in_(matches1)).limit(20).all()
+
+            # if the user has not rated anything or there are no predictions the user hasn't seen
+            # suggest any houses
+            # TODO: need better solution for the cold-start problem 
+            if not find_user or not predicted_houses: 
+                predicted_houses = House.query.limit(25).all()                 
+
             data = houses_schema.dumps(predicted_houses)
             j = json.JSONDecoder()
             data = j.decode(data)
+
             return {'status': 'success', "data": data}, 200 
         except Exception as e: 
             return {'status': str(e)}, 400
